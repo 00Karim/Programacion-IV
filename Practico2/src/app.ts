074f10fb9ad913aec619f16f1a9d05041669337f
00Karim <karimveleza@gmail.com>
2025-10-05 15:49:38 -0300

    Agregamos los test para la integracion E2E con supertest
    Agregamos los test con supertest para llegar a la fase de integracion E2E. Hicimos un archivo de app.ts y dentro de el pusimos un placeholder para las rutas que siempre lleva a error 501

--- Practico2/src/app.ts
+++ Practico2/src/app.ts
@@ -1,2 +1,40 @@
-// import express from "express"
-// import orderRoutes from "./"
+// rutas: 
+// POST order (crear un orden)
+// PUT order (cambiar el status de order por id)
+// GET order (get por id)
+// DELETE order (delete por id)
+// POST pizza
+
+import express from "express"
+// import orderRoutes from "./routes/orderRoutes"
+// import pizzaRoutes from "./routes/pizzaRoutes"
+
+// src/utils/placeholder.ts
+import { Request, Response } from "express";
+
+export function placeholder() {
+  return (_req: Request, res: Response) => {
+    res.status(501).json({ message: "Ruta no implementada todavía" });
+  };
+}
+
+const app = express()
+app.use(express.json())
+
+
+app.use("/order", placeholder())
+app.use("/pizza", placeholder())
+
+app.use((err,res) => { //Manejo implementacion de rutas
+    console.error(err)
+    res.status(501).json({error: "Error implementando la ruta solicitada"})
+})
+
+app.use((err,res) => { //Manejo basico de error 500
+    console.error(err)
+    res.status(500).json({error: "Error interno del servidor"})
+})
+
+export default app
+
+
