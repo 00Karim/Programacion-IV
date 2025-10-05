3e59644b3421093d8584e7a415690c0ff5f75977
00Karim <karimveleza@gmail.com>
2025-10-04 22:02:46 -0300

    Agregamos los esquemas para que los tests pasen a verde

--- Practico2/src/models/pizza.ts
+++ Practico2/src/models/pizza.ts
@@ -3,13 +3,13 @@ import { z } from "zod"
 const sizeEnum = ["S", "M", "L"]
 
 export const pizzaSchema = z.object({
-    // size: z.enum(sizeEnum, {
-    //     error: (issue) => //Issue se utiliza para recibir el tipo de error con Zod
-    //         issue.input === undefined //Si no se envio nada, recibe como input undefined
-    //             ? "El tamanio de la pizza es obligatorio"
-    //             : "El tamanio de la pizza debe ser S, M o L"
-    // }),
-    // toppings: z.array(z.string()).max(5, "El maximo de toppings permitidos es 5")
+    size: z.enum(sizeEnum, {
+        error: (issue) => //Issue se utiliza para recibir el tipo de error con Zod
+            issue.input === undefined //Si no se envio nada, recibe como input undefined
+                ? "El tamanio de la pizza es obligatorio"
+                : "El tamanio de la pizza debe ser S, M o L"
+    }),
+    toppings: z.array(z.string()).max(5, "El maximo de toppings permitidos es 5")
 })
 
 export type Pizza = z.infer<typeof pizzaSchema>
