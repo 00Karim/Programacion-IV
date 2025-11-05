c322230d3517921d88716409513d420eab711e5c
00Karim <karimveleza@gmail.com>
2025-11-05 17:08:58 -0300

    cambios tp3

--- Practico3/cafeteria/src/App.tsx
+++ Practico3/cafeteria/src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
-import { Menu } from "./Components/Menu";
+import { Menu } from "./Components";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [pedido, setPedido] = useState([]);
   return (
     <>
       <Menu></Menu>
--- Practico3/cafeteria/src/test/mocks/handlers.ts
+++ Practico3/cafeteria/src/test/mocks/handlers.ts
@@ -2,6 +2,8 @@ import { rest } from 'msw';
 
 export const handlers = [
   rest.get('http://localhost/api/menu', (req, res, ctx) => {
+    let request = req
+    request = request
     return res(
       ctx.status(200),
       ctx.json([
